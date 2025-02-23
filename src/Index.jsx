4b50481a83c47867c57f429010190c7a4f9967be
Saptha-Ghiri <104707149+Saptha-Ghiri@users.noreply.github.com>
2025-02-23 21:59:20 +0530

    index_up

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,7 +5,6 @@ import Navbar from "./Navbar";
 const LandingPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-      <Navbar />
 
       <div className="container mx-auto px-4 py-16">
         <div className="text-center mb-12">
